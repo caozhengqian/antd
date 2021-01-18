206f916519b6378ef2e807b4e7aa5e47c83eadd0
caozhengqian@deppon.com <caozhengqian@deppon.com>
2021-01-18 17:14:22 +0800

    add 初始化

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { Button } from 'ant-design-vue'; //引入组件，但不用引入样式
 
+Vue.component(Button.name, Button); //注册
 Vue.config.productionTip = false;
 
 new Vue({
